3f516a482e9ef3238be6623274f0a279d561d878
Felipe Lefèvre Marino <felipemarino91@gmail.com>
2017-11-07 00:28:19 -0200

    project created

---
+++ Adaptive-UI-Demo/Adaptive-UI-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Adaptive-UI-Demo
+//
+//  Created by Felipe Lefèvre Marino on 11/7/17.
+//  Copyright © 2017 Felipe Lefèvre Marino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
